fa37cf8ba282b6e3ac0580f0c3bd222b7c41c0ad
Austin <hillam.mail@gmail.com>
2015-10-31 22:57:45 -0400

    added communication to server for dumping and retrieving times at this time the server still has issues with csrf so these don't work yet

--- js/background.js
+++ js/background.js
@@ -1,6 +1,9 @@
 var sites = {};
+var sites_total = {};
 
 setInterval(query_tabs, 1000);
+setInterval(push_sites, 60000);
+setInterval(pull_sites, 60000);
 
 /*------------------------------------------------------------------------------
 	Get the current active tab, parse out the associated host, and increment
@@ -45,4 +48,33 @@ function update_popup(host, protocol){
 			//console.log(response);
 		}
 	);
+}
+
+/*------------------------------------------------------------------------------
+	SERVER URI
+------------------------------------------------------------------------------*/
+var domain = "http://localhost:3000/";
+
+/*------------------------------------------------------------------------------
+	Push current tracking data to the server.
+------------------------------------------------------------------------------*/
+function push_sites(){
+	var request = $.post(domain + 'sites', {sites: sites});
+
+	request.done(function(){
+		for(host in sites){
+			sites.host = 0;
+		}
+	});
+}
+
+/*------------------------------------------------------------------------------
+	Pull current tracking data from the server.
+------------------------------------------------------------------------------*/
+function pull_sites(){
+	var request = $.get(domain + 'sites');
+
+	request.done(function(data){
+		sites_total = data;
+	});
 }

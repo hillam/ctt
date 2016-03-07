40a4283faf361b9f8f407935fcdf21d5d8793e7f
Austin <hillam.mail@gmail.com>
2016-03-07 17:42:44 -0500

    static popup, get_user stub, update_sites funct, and time formatting

--- js/background.js
+++ js/background.js
@@ -1,12 +1,29 @@
+/*
+* sites: 		holds times (in sesconds) and get's dumped to the server and reset
+* 				to 0 every minute
+* sites_total: 	gets populated by the server upon pull_sites
+*/
 var sites = {};
 var sites_total = {};
 
+// Holds the URL for the current tab.
+// Used to initialize the popup without having to do an extra tab query.
+var current_tab = null;
+
 setInterval(query_tabs, 1000);
-setInterval(push_sites, 60000);
 
 // delay to make sure jquery is loaded before attempting
 setTimeout(pull_sites, 1000);
-setInterval(pull_sites, 60000);
+setInterval(update_sites, 60000);
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+	if(request.method == "get_current_tab"){
+		sendResponse({
+			host: current_tab,
+			time: (sites[current_tab] || 0) + (sites_total[current_tab] || 0)
+		});
+	}
+});
 
 /*------------------------------------------------------------------------------
 	Get the current active tab, parse out the associated host, and increment
@@ -24,8 +41,8 @@ function query_tabs(){
 			// IF HTTP OR HTTPS,
 			if(protocol == 'http' || protocol == 'https'){
 				increment(host);
-				update_popup(host, protocol);
 			}
+			current_tab = host;
 		}
 	});
 }
@@ -37,37 +54,34 @@ function increment(host){
 }
 
 /*------------------------------------------------------------------------------
-	Send message to popup.js signaling to update display.
-------------------------------------------------------------------------------*/
-function update_popup(host, protocol){
-	chrome.runtime.sendMessage(
-		{
-			method: "update",
-			host: host,
-			protocol: protocol,
-			time: (sites[host] || 0) + (sites_total[host] || 0)
-		},
-		function(response){
-			//console.log(response);
-		}
-	);
-}
-
-/*------------------------------------------------------------------------------
 	SERVER URI
 ------------------------------------------------------------------------------*/
 var domain = "http://ctt-ahill.rhcloud.com/";
 
 /*------------------------------------------------------------------------------
-	Push current tracking data to the server.
+	Push sites BEFORE pulling sites. Setting intervals for each does not
+	guarantee that they maintain this order.
 ------------------------------------------------------------------------------*/
-function push_sites(){
+function update_sites(){
+	push_sites(pull_sites);
+}
+
+/*------------------------------------------------------------------------------
+	Push current tracking data to the server.
+	- 	this should not be called on it's own.. use update_sites so that you get
+		a fresh copy of sites_total from the server after pushing up temp sites
+		values
+------------------------------------------------------------------------------*/
+function push_sites(callback){
 	var request = $.post(domain + 'sites', {sites: sites});
 
 	request.done(function(){
 		// reset times for all sites back to 0
 		for(var host in sites){
 			sites[host] = 0;
+		}
+		if(callback){
+			callback();
 		}
 	});
 }
@@ -80,5 +94,17 @@ function pull_sites(){
 
 	request.done(function(data){
 		sites_total = data;
+	});
+}
+
+/*------------------------------------------------------------------------------
+	Get user info.
+------------------------------------------------------------------------------*/
+function get_user(){
+	// TODO make route
+	var request = $.get(domain + 'user.json');
+
+	request.done(function(data){
+		// TODO do something
 	});
 }
--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,58 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
-	if(request.method == "update"){
-		if(request.protocol == 'http' || request.protocol == 'https'){
-			$('#site').text(request.host);
-			$('#time').text(request.time);
-		}
-		sendResponse('Popup has been updated.');
-	};
+$(document).ready(function(){
+	chrome.runtime.sendMessage({method: "get_current_tab"}, function(response){
+		$('#site').text(response.host);
+		$('#time').text(format_time(response.time));
+	});
 });
+
+// input: 	time in seconds
+// output:	"3.4 minutes", "3 weeks", "1 day", for example
+function format_time(time){
+	var units = [
+		{
+			name:		"year",
+			seconds: 	31536000
+		},
+		{
+			name:		"month",
+			seconds: 	2592000
+		},
+		{
+			name:		"week",
+			seconds: 	604800
+		},
+		{
+			name:		"day",
+			seconds: 	86400
+		},
+		{
+			name:		"hour",
+			seconds: 	3600
+		},
+		{
+			name:		"minute",
+			seconds: 	60
+		},
+		{
+			name:		"second",
+			seconds: 	1
+		}
+	];
+
+	for(var unit of units){
+		if(time >= unit.seconds){
+			return (time / unit.seconds).toFixed(is_divisible(time, unit.seconds)) + " " + unit.name +
+					Array(1 + is_equal(time, unit.seconds)).join('s'); // plural or not
+		}
+	}
+
+	// returns 0 if the numbers are equal, 1 otherwise
+	function is_equal(num1, num2){
+		return Math.abs(Math.sign((num1/num2).toFixed(1) - 1));
+	}
+
+	// returns 0 if the first number is divisible by the second, 1 otherwise
+	function is_divisible(num1, num2){
+		return Math.abs(Math.sign((num1/num2).toFixed(1) % 1));
+	}
+}
